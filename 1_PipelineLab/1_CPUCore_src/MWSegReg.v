767a65c31b4f7a39f9cf1dc52c7b4753b5d9e33c
NextoneX <2535049640@qq.com>
2023-05-06 15:52:54 +0800

    delete_test_file

--- 1_PipelineLab/1_CPUCore_src/MWSegReg.v
+++ 1_PipelineLab/1_CPUCore_src/MWSegReg.v
@@ -56,7 +56,7 @@ module MWSegReg(
     wire [31:0] RD_raw;
     DataRam DataRamInst (
         .clk    ( clk            ),  
-        .wea    ( (MemWriteE == 4'b1111) ? MemWriteE : (MemWriteE << AluOutMW[1:0])),             //sw的WE为1111，sb为0001，sh为0011，由控制模块生成
+        .wea    ( (MemWriteE == 4'b1111) ? MemWriteE : (MemWriteE << AluOutMW[1:0])), //sw的WE为1111，sb为0001，sh为0011，由控制模块生成
         .addra  ( AluOutMW[31:2]        ),  
         .dina   ( (MemWriteE == 4'b1111) ? ForwardData2 : (ForwardData2 << (AluOutMW[1:0]*8))),   
         .douta  ( RD_raw         ),
